f9165c8fbe5244ca7a22c736f7ad2f642507dcee
Kaido Kert <kaidokert@gmail.com>
2026-08-11 07:56:50 -0700

    Retire handled ISR interval samples

--- rm32/src/control/isr_logic.rs
+++ rm32/src/control/isr_logic.rs
@@ -29,6 +29,7 @@ pub fn ten_khz_tick<S: SharedComm, H: MotorHal>(ctx: &mut MotorContext<S, H>) {
     }
 
     let action = ctx.shared.isr_action();
+    let action_interval_count = ctx.hal.interval().count();
     match action {
         IsrAction::AllOff => {
             ctx.hal.phase().all_off();
@@ -36,10 +37,13 @@ pub fn ten_khz_tick<S: SharedComm, H: MotorHal>(ctx: &mut MotorContext<S, H>) {
             ctx.shared.clear_isr_action(IsrAction::AllOff);
             return;
         }
-        IsrAction::ResetIntervalTimer => {}
+        IsrAction::ResetIntervalTimer => {
+            ctx.hal.interval().set_count(0);
+            ctx.shared.clear_isr_action(IsrAction::ResetIntervalTimer);
+        }
         IsrAction::CommutateKick => {
             if ctx.shared.running() {
-                let count = ctx.hal.interval().count().min(u16::MAX as u32) as u16;
+                let count = action_interval_count.min(u16::MAX as u32) as u16;
                 let ci = ctx.shared.commutation_interval();
                 let new_ci = ctx.bemf.record_zero_cross(count, ci);
                 ctx.shared.set_commutation_interval(new_ci);
@@ -47,6 +51,8 @@ pub fn ten_khz_tick<S: SharedComm, H: MotorHal>(ctx: &mut MotorContext<S, H>) {
                     .com_timer()
                     .set_and_enable(COMMUTATE_KICK_DELAY_TICKS);
             }
+            ctx.hal.interval().set_count(0);
+            ctx.shared.clear_isr_action(IsrAction::CommutateKick);
         }
         IsrAction::None => {}
     }
@@ -176,14 +182,6 @@ pub fn ten_khz_tick<S: SharedComm, H: MotorHal>(ctx: &mut MotorContext<S, H>) {
     ctx.shared.set_forward(ctx.commutation.forward);
     ctx.shared
         .set_interval_timer_count(ctx.hal.interval().count());
-
-    if matches!(
-        action,
-        IsrAction::ResetIntervalTimer | IsrAction::CommutateKick
-    ) {
-        ctx.hal.interval().set_count(0);
-    }
-    ctx.shared.clear_isr_action(action);
 }
 
 /// BEMF polling (old_routine path).
